43d9ed360a13b53ad12f2e13b7e219367d53c34b
AYRUSAYAJ <notifi1jaya@gmail.com>
2025-10-22 10:51:09 +0530

    used gemini flash

--- api/perflexityApi.ts
+++ api/perflexityApi.ts
@@ -17,7 +17,7 @@ export const sendToGemini = async (
     };
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro:generateContent?key=${apiKey}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
